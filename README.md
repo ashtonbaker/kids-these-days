a3abb0e76062921d59bd6dd1679c14d521ff3ca1
Ashton.Baker <ashton.baker@ihsmarkit.com>
2018-08-07 12:20:22 -0400

    made all the changes

--- README.md
+++ README.md
@@ -1,26 +1,11 @@
-Cloud To Butt Plus
+Kids these Days
 =============
 
-Chrome extension that replaces occurrences of 'the cloud' with 'my butt', and other amusing cloud-related substitutions.
+Chrome extension that replaces occurrences of 'millennials' with 'kids these days'.
 
-[Direct download of crx file](https://github.com/hank/cloud-to-butt/blob/master/CloudToButt.crx?raw=true)
-
-Screenshot Gallery
-------------------
-
-http://www.flickr.com/groups/cloud-to-butt/
+[Direct download of crx file](https://github.com/ashtonbaker/kids-these-days/blob/master/KidsTheseDays.crx?raw=true)
 
 Installation
 ------------
 
 In Chrome, choose Window > Extensions.  Drag CloudToButt.crx into the page that appears.
-
-Safari Version
---------------
-
-Can be found here: https://github.com/logancollins/cloud-to-butt-safari
-
-Firefox Version
----------------
-
-Can be found here: https://github.com/DaveRandom/cloud-to-butt-mozilla
--- Source/content_script.js
+++ Source/content_script.js
@@ -33,31 +33,18 @@ function handleText(textNode) {
 	var v = textNode.nodeValue;
 
   // Deal with the easy case
-  v = v.replace(/\b(T|t)he (C|c)loud/g, function(match, p1, p2, offset, string) {
-    // t - 7 = m
-    // c - 1 = b
-    m = String.fromCharCode(p1.charCodeAt(0) - 7);
-    b = String.fromCharCode(p2.charCodeAt(0) - 1);
-    return m + "y " + b + "utt";
+  v = v.replace(/\b(M|m)illennials/g, function(match, p1, offset, string) {
+    // m - 2 = k
+    k = String.fromCharCode(p1.charCodeAt(0) - 2);
+    return k + "ids these days";
   });
 
-  // Deal with private clouds
-  v = v.replace(/\b(P|p)rivate (C|c)loud/g, function(match, p1, p2, offset, string) {
-    // c - 1 = b
-    b = String.fromCharCode(p2.charCodeAt(0) - 1);
-    return b + "utt";
+  v = v.replace(/\b(M|m)illennial/g, function(match, p1, offset, string) {
+    // m - 2 = k
+    k = String.fromCharCode(p1.charCodeAt(0) - 2);
+    return k + "id these days";
   });
-  // Get the corner cases
-  if(v.match(/cloud/i)) {
-    // If we're not talking about weather
-    if(v.match(/PaaS|SaaS|IaaS|computing|data|storage|cluster|distributed|server|hosting|provider|grid|enterprise|provision|apps|hardware|software|/i)) {
-      v = v.replace(/(C|c)loud/gi, function(match, p1, offset, string) {
-        // c - 1 = b
-        b = String.fromCharCode(p1.charCodeAt(0) - 1);
-        return b + "utt";
-      });
-    }
-  }
+
 	textNode.nodeValue = v;
 }
 
